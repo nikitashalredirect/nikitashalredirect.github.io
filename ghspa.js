ee11644d29767fc4490c8ada621e96a445b3099f
Nikita Shal <44640724+nikitashalredirect@users.noreply.github.com>
2018-11-01 15:58:06 +0300

    Update ghspa.js

--- ghspa.js
+++ ghspa.js
@@ -14,19 +14,7 @@
   
    /* resolve 404 redirects into internal routes */
    function resolve() {
-     if (l.search) {
-       var q = {}
-       l.search.slice(1).split('&').forEach(function(v) {
-         var a = v.split('=')
-         q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&')
-       })
-       if (q.p !== undefined) {
-         window.history.replaceState(null, null,
-           repo + (q.p || '') +
-           (q.q ? ('?' + q.q) : '') +
-           l.hash
-         )
-       }
+      window.location.href = "/";
      }
    }
 
